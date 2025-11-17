5055117927a20ab08bd1c520e6e7c987df23d37c
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 13:32:46 +0100

    Delete HExX-XD.py

--- HExX-XD.py
+++
@@ -1,9 +0,0 @@
-
-# ملف تشغيل للوحدة المشفرة HExX.cpython-312.so
-import HExX
-
-# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
-if hasattr(HExX, 'main'):
-    iAmMain().iAmMenu()
-else:
-    print("تم استيراد HExX بنجاح، ولكن لا توجد دالة main للتشغيل.")
