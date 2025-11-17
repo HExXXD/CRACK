d7e8f6b8c75dd802f1482634a81b74c8d1039ad1
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 18:42:26 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -1,21 +1,11 @@
-import platform
-import sys
+import os, platform, time, sys
 
-arch = platform.machine()
+print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mChecking For Update...')
 
-try:
-    if arch in ["aarch64", "arm64", "armv8l"]:
-        import Pylnit_hexx_64 as HExX
-    elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
-        import Pylnit_hexx_32 as HExX
-    else:
-        print("Architecture not supported:", arch)
-        sys.exit()
-except ImportError as e:
-    print("Import error:", e)
-    sys.exit()
-
-if hasattr(HExX, "main"):
-    HExX.main()
-else:
-    print("No main() found in module.")
+hexx = platform.architecture()[0]
+if hexx == '64bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Device is 64bit')
+ import hexx_64
+elif hexx == '32bit':
+ print('\033[1;97m[\033[1;91m+\033[1;91m] \033[1;97mYour Devive is 32bit')
+ import hexx_32
