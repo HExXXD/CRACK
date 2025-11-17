1e870abc143be9ed9c3bd73492dae39448a06d1d
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 13:28:01 +0100

    Add files via upload

---
+++ HExX-XD.py
@@ -0,0 +1,9 @@
+
+# ملف تشغيل للوحدة المشفرة HExX.cpython-312.so
+import HExX
+
+# تأكد أن الدالة الرئيسية موجودة داخل الملف الأصلي
+if hasattr(HExX, 'main'):
+    HExX.main()
+else:
+    print("تم استيراد HExX بنجاح، ولكن لا توجد دالة main للتشغيل.")
