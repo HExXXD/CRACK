45b4f51c973ec7c787cc5c136458dd589ba31336
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 19:16:06 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -4,12 +4,12 @@ white = '\x1b[1;37m'
 rad = '\x1b[38;5;160m'
 green = '\x1b[38;5;46m'
 print(f' {rad}[{white}◆{rad}] {green}Checking For Update...')
-arch = platform.machine()
+hexx = platform.architecture()[0]
 
-if arch in ["aarch64", "arm64", "armv8l"]:
+if hexx == '64bit':
     print(f' {rad}[{white}◆{rad}] {green}Your Device is 64bit')
     import hexx_64 as HExX
-elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
+elif hexx == '32bit':
     print(f' {rad}[{white}◆{rad}] {green}Your Device is 32bit')
     import hexx_32 as HExX
 else:
