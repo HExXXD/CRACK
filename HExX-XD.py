0ab97264ea6cbdfd1f948445336d6ff4f404374f
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 18:58:13 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -1,11 +1,16 @@
 import platform
 import sys
-
+white = '\x1b[1;37m'
+rad = '\x1b[38;5;160m'
+green = '\x1b[38;5;46m'
+print(f' {rad}[{white}◆{rad}] {green}Checking For Update...')
 arch = platform.machine()
 
 if arch in ["aarch64", "arm64", "armv8l"]:
+    print(f' {rad}[{white}◆{rad}] {green}Your Device is 64bit')
     import hexx_64 as HExX
 elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
+    print(f' {rad}[{white}◆{rad}] {green}Your Device is 32bit')
     import hexx_32 as HExX
 else:
     print("Architecture not supported:", arch)
