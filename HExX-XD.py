6b666d372260fc228a4fa481f0e8a850e47bf0d6
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 18:39:20 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -1,14 +1,18 @@
-import platform 
+import platform
 import sys
 
 arch = platform.machine()
 
-if arch in ["aarch64", "arm64", "armv8l"]:
-    import hexx_64 as HExX
-elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
-    import hexx_32 as HExX
-else:
-    print("Architecture not supported:", arch)
+try:
+    if arch in ["aarch64", "arm64", "armv8l"]:
+        import Pylnit_hexx_64 as HExX
+    elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
+        import Pylnit_hexx_32 as HExX
+    else:
+        print("Architecture not supported:", arch)
+        sys.exit()
+except ImportError as e:
+    print("Import error:", e)
     sys.exit()
 
 if hasattr(HExX, "main"):
