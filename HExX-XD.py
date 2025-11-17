acc8dbaccb952ab47b36a6d604021d8475ed6379
HExX <153182049+HExXXD@users.noreply.github.com>
2025-11-17 18:54:49 +0100

    Update HExX-XD.py

--- HExX-XD.py
+++ HExX-XD.py
@@ -1,13 +1,17 @@
-import os, platform, time, sys
-white = '\x1b[1;37m'
-rad = '\x1b[38;5;160m'
-green = '\x1b[38;5;46m'
-print(f' {rad}[{white}◆{rad}] {green}Checking For Update...')
+import platform
+import sys
 
-hexx = platform.architecture()[0]
-if hexx == '64bit':
- print(f' {rad}[{white}◆{rad}] {green}Your Device is 64bit')
- import hexx_64
-elif hexx == '32bit':
- print(f' {rad}[{white}◆{rad}] {green}Your Devive is 32bit')
- import hexx_32
+arch = platform.machine()
+
+if arch in ["aarch64", "arm64", "armv8l"]:
+    import hexx_64 as HExX
+elif arch in ["armv7l", "arm", "armeabi", "armeabi-v7a"]:
+    import hexx_32 as HExX
+else:
+    print("Architecture not supported:", arch)
+    sys.exit()
+
+if hasattr(HExX, "main"):
+    HExX.main()
+else:
+    print("No main() found in module.")
